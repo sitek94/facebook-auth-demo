7b3d7f155a69c5c1ad12132919cc3a1c72361a20
Maciek <msitkowski94@gmail.com>
2022-08-12 22:35:01 +0200

    init fb sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  React.useEffect(() => {
+    FB.getLoginStatus(function (response) {
+      console.log(response)
+    })
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <div
+        className="fb-login-button"
+        data-width="320"
+        data-size="large"
+        data-button-type="login_with"
+        data-layout="rounded"
+        data-auto-logout-link="true"
+        data-use-continue-as="true"
+      ></div>
     </div>
   )
 }
-
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    https: {
+      key: fs.readFileSync(path.join(__dirname, '.certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, '.certs/localhost.pem')),
+    },
+  },
 })
